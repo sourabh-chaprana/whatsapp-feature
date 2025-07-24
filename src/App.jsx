1d83baa5833352bfd80b516cdc44b9246fce350c
Roshan Kumar <Roshan.KumarC@aionos.ai>
2025-07-24 10:26:04 +0530

    added facebook login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import CommunicationChannel from "./whatsappModule/BotManagement/CommunicationChannel";
+import WhatsappSettings from "./facebook/WhatsappSettings";
 
 function App() {
   return (
     <div className="app-container">
       <Routes>
         <Route path="/" element={<CommunicationChannel />} />
+        <Route path="/whatsapp-settings" element={<WhatsappSettings />} />
       </Routes>
     </div>
   );
